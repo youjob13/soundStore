592697cc825881259478b90e134b695bcf03a21d
Danil Rodionow <danil.rodionow13@gmail.com>
2020-10-09 21:20:43 +0300

    fix

--- src/modules/sliderProduct.js
+++
@@ -1,8 +0,0 @@
-import getPhoto from './getPhoto';
-
-const sliderProduct = (selectSlide = 0) => {
-    const slides = document.querySelector('.product-other-item').children;
-    
-};
-
-export default sliderProduct;
